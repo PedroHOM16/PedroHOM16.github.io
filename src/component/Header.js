2432b5918873877eb78374bb36cf0c8afe368c81
pedro <pedro.admoura@gmail.com>
2023-01-30 01:38:01 -0300

    arruma botoes de language

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,8 +4,8 @@ import '../index.css'
 import PortContext from '../context/PortContext';
 // import logo from '../images/foto-pessoal.jpeg';
 import T from '../style/tailwind'
-import eua from "../images/icons/eua.jpeg";
-const brasil = require("../images/icons/brasil.png");
+import eua from "../images/icons/eua.png";
+import brasil from "../images/icons/brasil.png";
 
 export default function Header() {
   // const { clientName, language } = useContext(PortContext);
@@ -13,38 +13,42 @@ export default function Header() {
   // const clientName = localStorage.getItem('clientName');
   const { language, setLanguage, clientName, setClientName } = useContext(PortContext);
 
-  const handleLangChange = ({ target: { value } }) => {
-    setLanguage(value);
+  const handleLangChangePort = ({ target: { value } }) => {
+    setLanguage("Port");
+  }
+
+  const handleLangChangeEng = ({ target: { value } }) => {
+    setLanguage("Eng");
   }
 
   const handleChange = ({ target: { value } }) => {
     setClientName(value);
   }
 
-  console.log("img brasil : ", brasil.slice(10))
+  console.log("img brasil : ", language)
 
     return (
       <div className={T.header.App}>
         <h3 className={ T.header.Tittle }>Pedro Henrique Moura</h3>
         <div className={T.header.Label}>
-          <select
+          {/* <select
             value={language}
             onChange={handleLangChange}
             className={T.header.LanguageSelect}
           >
-            <option className={T.header.OptionsPor} value='Port' >Português</option>
-            <option className={T.header.OptionsEng} value='Eng'>English</option>
-          </select>
+          </select> */}
+        </div>
+        <p className={T.header.Subtittle}>
+          <button className={T.header.OptionsPor} value='Port' onClick={handleLangChangePort}>{<img src={brasil} />}</button>
+          <button className={T.header.OptionsEng} value='Eng' onClick={handleLangChangeEng}>{<img src={eua} />}</button>
+             Hey {clientName}, {language === "Port" ? "você é bem vindo(a)" : "you are welcome here" } ! &#128526;
+        </p>
           <input
             className={T.header.NameInput}
-            placeholder={language === "Port" ? "Digite seu nome" : "Type your name"}
+            placeholder={language === "Port" ? "seu nome ?" : "your name ?"}
             onChange={handleChange}
             value={clientName}
           />
-        </div>
-        <p className={T.header.Subtittle}>
-          Hey {clientName}, {language === "Port" ? "você é bem vindo(a)" : "you are welcome here" } ! &#128526;
-        </p>
         <div className={T.header.NavBar}>
           <Link className={T.header.NavItem} to="/home">{language === "Port" ? <p>Iníci&#8962;</p> : <p>H&#8962;me</p> }</Link>
           <Link className={T.header.NavItem} to="/havfun">{language === "Port" ? <p>Divirta-se</p> : <p>HaveFun</p> }</Link>
